2ca04a5d9538ba4b516d82d860afc3d59bfe74f4
Mohamed-Alaa <mohamed.alaa.el.din.hassan@gmail.com>
2018-05-21 14:51:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ControlpanelComponent } from './controlpanel/controlpanel.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'reservation', component: ReservationComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'controlpanel', component: ControlpanelComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReservationComponent,
+    CheckoutComponent,
+    ControlpanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot(
+          appRoutes,
+          { enableTracing: false } // <-- debugging purposes only
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
